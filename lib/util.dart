c6f86d30f6caa120a532f55b12abc0dd1f829562
mohammdpc <mohammdpc2018@gmail.com>
2026-01-28 21:22:10 +0200

    Initial commit

---
+++ lib/util.dart
@@ -0,0 +1,19 @@
+import 'dart:ui';
+
+Color mainColor = Color(0xff535862);
+Color disable = Color(0xffB9B9B9);
+Color mainText = Color(0xff1C1C1C);
+Color secText = Color(0xff686868);
+Color background = Color(0xffF4F7FF);
+Color input = Color(0xffFFFFFF);
+Color stroke = Color(0xffF0F0F0);
+Color red = Color(0xffFF3232);
+
+//dark
+
+Color darkMainColor = Color(0xff457AED);
+Color darkMainText = Color(0xffFFFFFF);
+Color darkSecText = Color(0xffD6D6D6);
+Color darkBackground = Color(0xff000F30);
+Color darkInput = Color(0xff001440);
+Color darkStroke = Color(0xff002D8F);
